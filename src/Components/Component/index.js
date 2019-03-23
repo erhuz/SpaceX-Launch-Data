c3c962a237b3af8ca594c05ba6786b7d71e34c69
Benjamin Fransson <benjamin@cafcon.se>
2019-03-23 12:28:42 +0100

    Added component template

---
+++ src/Components/Component/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import StyledComponent from './styles';
+
+const Component = () => {
+  return(
+    <StyledComponent>
+        
+    </StyledComponent>
+  );
+}
+
+export default Component;
