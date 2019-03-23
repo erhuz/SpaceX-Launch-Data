3891c7bd73811a8f0f0e2de083e93870da08aa6e
Benjamin Fransson <benjamin@cafcon.se>
2019-03-23 12:28:23 +0100

    Created Button component

---
+++ src/Components/Button/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import StyledButton from './styles';
+
+const Button = StyledButton;
+
+export default Button;
---
+++ src/Components/Button/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  padding: 8px 16px;
+  font-size: 16px;
+  font-weight: bold;
+  border: none;
+  border-radius: 5px;
+
+  background-color: #4286f4;
+  color: #fff;
+`;
+
+export default StyledButton;
