13c28d930162a16ecca92d672b41c149e3049d75
Benjamin Fransson <benjamin@cafcon.se>
2019-03-25 21:47:42 +0100

    Added launch component

---
+++ src/Components/Launch/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import StyledLaunch from './styles';
+
+const Launch = () => {
+  return(
+    <StyledLaunch>
+        
+    </StyledLaunch>
+  );
+}
+
+export default Launch;
---
+++ src/Components/Launch/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const StyledLaunch = styled.div`
+  
+`;
+
+export default StyledLaunch;
