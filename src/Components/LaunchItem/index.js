52c94e7da9d4c6291f6c1c8d3ad39f0b707771fa
Benjamin Fransson <benjamin@cafcon.se>
2019-03-25 21:47:56 +0100

    Added LaunchItem (list item)

---
+++ src/Components/LaunchItem/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import StyledLaunchItem from './styles';
+
+const LaunchItem = () => {
+  return(
+    <StyledLaunchItem>
+        
+    </StyledLaunchItem>
+  );
+}
+
+export default LaunchItem;
---
+++ src/Components/LaunchItem/styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const StyledLaunchItem = styled.div`
+  
+`;
+
+export default StyledLaunchItem;
