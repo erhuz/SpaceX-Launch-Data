60a3fc4f661f952eb04ef4ffecd7256f1f73011d
Benjamin Fransson <benjamin@cafcon.se>
2019-03-26 13:59:17 +0100

    Added props to display in each item

--- src/Components/LaunchItem/index.js
+++ src/Components/LaunchItem/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import StyledLaunchItem from './styles';
 
-const LaunchItem = () => {
+const LaunchItem = (props) => {
+
+  const date = new Date(props.launch_date);
+
+
   return(
-    <StyledLaunchItem>
-        
+    <StyledLaunchItem >
+        <td>{props.flight_number}</td>
+        <td>{props.mission_name}</td>
+        <td>{date.toDateString()}</td>
     </StyledLaunchItem>
   );
 }
 
 export default LaunchItem;
+
+// LaunchList.propTypes = {
+  
+// }
