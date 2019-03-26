ee8becfec76afc9616f36eb41cbba701bf25d9b0
Benjamin Fransson <benjamin@cafcon.se>
2019-03-26 13:59:52 +0100

    Added content to LaunchList

--- src/Components/LaunchList/index.js
+++ src/Components/LaunchList/index.js
@@ -1,22 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import StyledLaunchList from './styles';
+import LaunchItem from '../LaunchItem';
 
 const LaunchList = (props) => {
   return(
     <StyledLaunchList>
+      <thead>
+        <tr>
+          <th>Flight ID</th>
+          <th>Mission Name</th>
+          <th>Launch Date</th>
+        </tr>
+      </thead>
+      <tbody>
         {
-          props.items.map(() => )
+          props.items.map((item, index) => {
+            return (
+              <LaunchItem
+              key={index}
+              flight_number={item.flight_number}
+              mission_name={item.mission_name}
+              launch_date={item.launch_date_utc}
+              />
+            )
+          })
         }
+      </tbody>
     </StyledLaunchList>
   );
 }
-
-export default LaunchList;
-
-
-<ul>
-  {elements.map((value, index) => {
-    return <li key={index}>{value}</li>
-  })}
-</ul>
+    
+    export default LaunchList;
+    
+    // LaunchList.propTypes = {
+    
+    // }
