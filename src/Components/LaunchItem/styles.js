6bf9bdffdf19c0d49e2646936e33390ed1b0cd60
Benjamin Fransson <benjamin@cafcon.se>
2019-03-26 13:59:29 +0100

    Added styling for LaunchItems

--- src/Components/LaunchItem/styles.js
+++ src/Components/LaunchItem/styles.js
@@ -1,7 +1,16 @@
 import styled from 'styled-components';
 
-const StyledLaunchItem = styled.div`
+const StyledLaunchItem = styled.tr`
+  padding: 12px;
   
+  &:not(:last-child){
+    border-bottom: 1px solid #222;
+  }
+
+  &:hover {
+    border: 1px solid #333;
+    cursor: pointer;
+  }
 `;
 
 export default StyledLaunchItem;
