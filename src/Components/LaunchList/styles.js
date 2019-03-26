60d867efb9da9f8afabebdb976afaf81adbd51bf
Benjamin Fransson <benjamin@cafcon.se>
2019-03-26 13:59:40 +0100

    Added table styling

--- src/Components/LaunchList/styles.js
+++ src/Components/LaunchList/styles.js
@@ -1,7 +1,25 @@
 import styled from 'styled-components';
 
-const StyledLaunchList = styled.ul`
-  
+const StyledLaunchList = styled.table`
+  margin: 0 auto;
+
+  th{
+    text-align: left;
+  }
+
+  td, th {
+    padding: 8px 16px;
+  }
+
+  tr:nth-child(even){
+    background-color: #22222203
+  }
+
+  tr:hover {
+    background-color: #22222208
+
+    
+  }
 `;
 
 export default StyledLaunchList;
